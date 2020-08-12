f02d6d7a13ac09f304ba2908b494a1853b69316c
Evyatar Haim <evyataros1@gmail.com>
2020-08-12 12:23:46 +0300

    first commit

---
+++ src/Components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import buildControls from '../BuildControls';
+
+const buildControl = (props) => (
+      <div>
+          <div></div>
+      </div>
+)
+
+export default buildControls;
---
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BuildControls.css';
+
+ const buildControls = (props) => (
+            <div>
+                  
+            </div>
+        )
+    
+ 
+
+export default buildControls;
