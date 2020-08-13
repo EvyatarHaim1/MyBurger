c9bfddcc8b7a6efc731f9eb5930b180d4d6f7192
Evyatar Haim <evyataros1@gmail.com>
2020-08-13 13:37:57 +0300

    adding ingredient to the custom burger works

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -6,12 +6,16 @@ const controls = [
 	{ label: 'Salad', type: 'salad' },
 	{ label: 'Bacon', type: 'bacon' },
 	{ label: 'Cheese', type: 'cheese' },
-	{ label: 'Meat', type: 'meat' },
+	{ label: 'Meat', type: 'meat' }
 ];
 
 const buildControls = (props) => (
 	<div className={classes.BuildControls}>
-		{controls.map((ctrl) => <BuildControl key={ctrl.label} label={ctrl.label} />)}
+		{controls.map((ctrl) => (
+            <BuildControl key={ctrl.label} 
+                          label={ctrl.label} 
+                          added={() => props.ingredientAdded(ctrl.type)} />
+		))}
 	</div>
 );
 
