db5e273360777b9a2b4e7f2f090ec74931553eb8
Evyatar Haim <evyataros1@gmail.com>
2020-08-13 13:15:02 +0300

    buildControls view is ready below the burger without logic yet

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../Components/Burger/Burger';
-
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component {
     // constructor(props){
     //     super(props);
@@ -19,7 +19,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls />
             </Aux>
         )
     }
