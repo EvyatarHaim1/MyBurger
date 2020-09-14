abacf132595623409d50e72a06e254cbeffb5c2b
Evyatar Haim <evyataros1@gmail.com>
2020-09-14 19:05:29 +0300

    order submission & passing data between pages

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
+    baseURL: 'https://react-my-burger-40e0c.firebaseio.com/'
 });
 
 export default instance;
