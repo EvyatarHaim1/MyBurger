45cdcd634b7898e19bb74a01bc92b5ed3e1d3ce8
Evyatar Haim <evyataros1@gmail.com>
2020-09-16 21:31:35 +0300

    improving visual feedback of the form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,6 +15,11 @@ import Input from '../../../components/UI/Input/Input';
                         placeholder: 'Your Name'
                     },
                     value: '',
+                    validation: {
+                        required: true
+                    },
+                    valid: false,
+                    touched: false
                 },
                 street:  {
                     elementType:'input',
@@ -23,6 +28,11 @@ import Input from '../../../components/UI/Input/Input';
                         placeholder: 'Street'
                     },
                     value: '',
+                    validation: {
+                        required: true
+                    },
+                    valid: false,
+                    touched: false
                 },
                 zipCode:  {
                     elementType:'input',
@@ -31,6 +41,13 @@ import Input from '../../../components/UI/Input/Input';
                         placeholder: 'ZIP Code'
                     },
                     value: '',
+                    validation: {
+                        required: true,
+                        minLength: 5,
+                        maxLength: 5
+                    },
+                    valid: false,
+                    touched: false
                 },
                 country:  {
                     elementType:'input',
@@ -39,6 +56,11 @@ import Input from '../../../components/UI/Input/Input';
                         placeholder: 'Country'
                     },
                     value: '',
+                    validation: {
+                        required: true
+                    },
+                    valid: false,
+                    touched: false
                 },
                 email:  {
                     elementType:'input',
@@ -47,6 +69,11 @@ import Input from '../../../components/UI/Input/Input';
                         placeholder: 'Email'
                     },
                     value: '',
+                    validation: {
+                        required: true
+                    },
+                    valid: false,
+                    touched: false
                 },
                 deliveryMethod:  {
                     elementType:'select',
@@ -59,6 +86,20 @@ import Input from '../../../components/UI/Input/Input';
         },
          loading: false
      }
+
+    checkValidity = (value, rules) => {
+      let isValid = true;
+      if (rules.required ){
+          isValid = value.trim() !=='' && isValid;
+      }
+      if (rules.minLength){
+          isValid = value.length >= rules.minLength && isValid
+      }
+      if (rules.maxLength){
+        isValid = value.length <= rules.maxLength && isValid
+    }
+      return isValid;
+    }
 
     orderHandler = (event) => {
     event.preventDefault();
@@ -90,7 +131,10 @@ import Input from '../../../components/UI/Input/Input';
           ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
-        updatedOrderForm[inputIdentifier] = updatedFormElement
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.touched = true;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
         this.setState({orderForm: updatedOrderForm});
     }
 
@@ -110,6 +154,9 @@ import Input from '../../../components/UI/Input/Input';
                    elementType={formElement.config.elementType}
                    elementConfig={formElement.config.elementConfig}
                    value={formElement.config.value}
+                   invalid={!formElement.config.valid}
+                   shouldValidate = {formElement.config.validation}
+                   touched={formElement.config.touched}
                    changed={(event)=> this.inputChangedHandler(event, formElement.id)}/>
         ))}
         <Button btnType="Success" clicked={this.orderHandler}> ORDER </Button>
